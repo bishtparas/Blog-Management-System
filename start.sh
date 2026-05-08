ea490189741824b14a58c77219c474724da4a1a6
bishtparas <bishtparas@users.noreply.github.com>
2026-05-08 19:50:58 +0530

    Auto run seeders on startup

--- start.sh
+++ start.sh
@@ -5,8 +5,9 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Run database migrations
+# Run database migrations and seeders
 php artisan migrate --force
+php artisan db:seed --force
 
 # Start Apache in foreground
 apache2-foreground
